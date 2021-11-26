1cc2d26ecb415cc8407d38de3874349acfc7c8d4
Angel <coelhopereira@et.intechinfo.fr>
2021-11-26 16:21:28 +0100

    index

--- index.php
+++ index.php
@@ -3,18 +3,6 @@ session_start();
 
 	try 
 	{
-<<<<<<< HEAD
-		if (!isset($_GET['action']))
-		{
-			include('./controller/login.php');
-		}
-		else 
-		{
-			switch ($_GET["action"]) {
-                case '':
-                    include ();
-                    break;
-=======
 			switch ($_GET["action"]) {
 				case  'accueil':
 					include("conexion.php");
@@ -27,4 +15,3 @@ session_start();
 		echo 'Erreur : '. $e->getMessage();
 	}
 ?>
->>>>>>> fdac2c153ad0a3b40701be02ec92373860009b7a
