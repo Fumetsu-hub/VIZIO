1bafb2421541447d80ccae63bb613ae8965392d7
Ruben <rubenh4502@gmail.com>
2021-11-26 16:13:34 +0100

    v1.1

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+<head>
+ <title>test</title>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- --------------------------------------BARRE DE NAVIGATION------------------------------------------------ -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid couleur">
+    <a class="navbar-brand" href="#">Accueil</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Exemple2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dossiers Patient</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Questionnaires</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Exemple</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- -------------------------------------TEST------------------------------------------------- -->
+
+
+
+<!-- -------------------------------------FORMULAIRE------------------------------------------------- -->
+
+<br><br> 
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="container">
+                <div class="mb-3 row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-sm-6"></div>
+    </div>
+</div>
+
+<!-- -------------------------------------------------------------------------------------- -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
