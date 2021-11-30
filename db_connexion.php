45c667b085d21877ea63751990a330673b0bc5ba
Fumetsu-hub <angelcp38@gmail.com>
2021-11-30 10:25:45 +0100

    db connect

--- db_connexion.php
+++ db_connexion.php
@@ -1,32 +1,17 @@
 <?php
-//connexion a la bdd
-//***en attente de la bdd */
-class DB{
-    private $host ='localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'shop';
-    private $db;
 
-    public function __construct($host = null, $username = null, $password = null, $database = null){
-        if($host != null){
-            $this->host = $host;
-            $this->username = $username;
-            $this->password = $password;
-            $this->database = $database;
-
+    function db_connect()
+    {
+        try
+        {
+            $pdo = new PDO('mysql:host=localhost;dbname=vizio','root','');
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+            //echo 'connextion réussie';
+            return $pdo;
         }
-        try{
-        $this->db = new PDO('mysql:host='.$this->host.';dbname='.$this->database, $this->username, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
-    PDO::ATTR_ERRMODE=> PDO::ERRMODE_WARNING));
-        }catch(PDOException $e){
-            die('base non liée');
+        catch (PDOException $e)
+        {
+            echo "erreur de connexion à la base de donnée";
         }
     }
-    public function query($sql, $data= array()){
-        $recuperer = $this->db->prepare('SELECT * FROM produit');
-$recuperer->execute();
-return $recuperer->FetchAll(PDO::FETCH_OBJ);
-    }
-}
-?>
+    ?>
