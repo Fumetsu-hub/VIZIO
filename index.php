1c170a87a66a0a4be65f04f477a4f0bf27d6aed8
Fumetsu-hub <angelcp38@gmail.com>
2021-11-30 11:24:29 +0100

    angel

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ session_start();
 
 	try 
 	{
+		if (!isset($_GET['action']))
+		{
+			include('./controller/login.php');
+		}
+		else 
+		{
 			switch ($_GET["action"]) {
 				case  'LOGIN':
 					include("/controlleur/login.php");
