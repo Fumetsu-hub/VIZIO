2457b301d5b6cb5611375ba4ab00ec79fad5081a
Angel <coelhopereira@et.intechinfo.fr>
2021-12-06 10:41:58 +0100

    angel

--- controlleur/login.php
+++ controlleur/login.php
@@ -2,26 +2,6 @@
 
     require_once('./model/login.php');
 
-    if (isset($_POST['mdp']) && $_POST['mail'])
-    {
-        if (user_check($_POST['mdp'] ,$_POST['mail']))
-        {
-            //echo 'connecté';
-
-            $client = user_info($_POST['mail']);
-
-            foreach(array('id_user','nom', 'prenom', 'mail') as $key )
-            {
-                $_SESSION[$key] = $client[$key];
-            }
-            
-            header('Location: index.php?action=accueil');
-            exit();
-        }
-        else 
-            $erreur = "Le nom d'utilisateur ou le Mot de Pass est Incorrect";
-        }
-
     if(isset($_POST['action']))
     {
         if(empty($_POST['mail']) AND empty($_POST['mdp']))
