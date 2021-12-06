321ff4f09badd4c4c40739d3968a43f60a212ed6
Ruben <rubenh4502@gmail.com>
2021-12-06 15:11:53 +0100

    ruben

--- controlleur/sign_up.php
+++ controlleur/sign_up.php
@@ -4,8 +4,7 @@
 
     if(isset($_POST['action']))
     {
-        if(empty($_POST['email']) OR empty($_POST['mdp']) OR empty($_POST['nom']) OR empty($_POST['mdp2']) 
-        OR empty($_POST['age']) OR empty($_POST['sex']) OR empty($_POST['tel']) OR empty($_POST['statut']))
+        if(empty($_POST['email']))
         {
                 
             $erreur2 = "Veuillez remplir tous les champs !";
--- model/sign_up.php
+++ model/sign_up.php
@@ -11,7 +11,6 @@ mysqli_select_db($base, "vizio");
  if(isset($_POST['action']))
  {
    if(isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['mdp']) and isset($_POST['mdp2']) and isset($_POST['email']) and isset($_POST['age']) and isset($_POST['sex']) and isset($_POST['tel']) and isset($_POST['statut']))
-    
      {
        if(!empty($_POST['nom']) and !empty($_POST['prenom']) and !empty($_POST['mdp']) and !empty($_POST['mdp2']) and !empty($_POST['email']) and !empty($_POST['age']) and !empty($_POST['sex']) and !empty($_POST['tel']) and isset($_POST['statut']))  
      {
@@ -24,7 +23,7 @@ mysqli_select_db($base, "vizio");
          $sex = htmlspecialchars($_POST['sex']);
          $tel = htmlspecialchars($_POST['tel']);
          $statut = htmlspecialchars($_POST['statut']);
-     } 
+     
      if($mdp == $mdp2)
      {
         $sql = 'INSERT INTO user VALUES("", "'.$nom.'", "'.$prenom.'", "'.$mdp.'", "'.$mail.'", "'.$age.'", "'.$sex.'", "'.$tel.'", "'.$statut.'")';
@@ -35,6 +34,7 @@ mysqli_select_db($base, "vizio");
      {
          $erreur = "Vos mots de passe ne correspondent pas !";
      }
+    }
         
     }
 
