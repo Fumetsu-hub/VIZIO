b5fb411d6904efe33daa800501d29162c0f35231
Bryan2607 <btran2607@gmail.com>
2021-12-06 10:24:17 +0100

    modif

--- model/supprimer_profil.php
+++ model/supprimer_profil.php
@@ -3,9 +3,8 @@ include('db_connexion.php');
     //suuprimer
     if(isset($_POST['delete']))
     {
-        $requete= $DB->query("DELETE FROM user WHERE : id = id");
+        $requete= $DB->query("DELETE FROM user WHERE : id = :id");
         $_SESSION['flash_message'] = "Ce compte à été supprimé";
         exit;
     }
-    
 ?>
