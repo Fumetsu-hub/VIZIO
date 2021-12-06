9cec5002421961ce924112dcea5fce57f4cdf7a6
elmadj <elmadjidyoussoufa@live.fr>
2021-12-06 10:46:18 +0100

    log

--- controlleur/login.php
+++ controlleur/login.php
@@ -11,8 +11,8 @@
             <form action="verification.php" method="POST">
                 <h1>Connexion</h1>
                 
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+                <label><b>email</b></label>
+                <input type="mail" placeholder="Entrer le mail" name="email" required>
 
                 <label><b>Mot de passe</b></label>
                 <input type="password" placeholder="Entrer le mot de passe" name="password" required>
