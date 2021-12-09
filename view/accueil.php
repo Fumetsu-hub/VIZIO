267e81ca7b65960e35a80494f65000365ff69059
Fumetsu-hub <angelcp38@gmail.com>
2021-12-09 10:57:51 +0100

    angel

--- view/accueil.php
+++ view/accueil.php
@@ -11,6 +11,12 @@ include('./utilitaire/bootstrap.php');
 include('./utilitaire/barre_de_navigation.php');
 ?>
 
+<form action="./index.php" method="GET">
+            <button type="submit" value="profil" name="action" class="btn btn-primary">Profil</button>
+    </form><br><br>
+    <form action="./index.php" method="GET">
+            <button type="submit" value="creer_dossier_patient" name="action" class="btn btn-primary">Crée un dossier patient</button>
+    </form><br><br>
     <form action="./index.php" method="GET">
             <button type="submit" value="consultation" name="action" class="btn btn-primary">Crée une fiche consultation</button>
     </form><br><br>
