31a43b9ba0206e0a5ed70c4f6916ed5a9e0a18e8
Fumetsu-hub <angelcp38@gmail.com>
2021-12-09 10:47:53 +0100

    angel

--- view/accueil.php
+++ view/accueil.php
@@ -11,48 +11,11 @@ include('./utilitaire/bootstrap.php');
 include('./utilitaire/barre_de_navigation.php');
 ?>
 
-
-    <br><br><br>
-
-    <form action="./index.php" method="POST">
-        <div class="topnav1">
-            <form id="profil" action="./index.php" method='GET'>
-                <input type="button" name="action" value="profil" />
-            </form>
-            
-        </div>
-    </form>
-
-    <br><br>
-
-    <form action="./index.php" method="POST">
-        <div class="topnav2">
-            <form id="liste patient" action="./index.php" method='GET'>
-                <input type="button" name="action" value="liste patient" />
-            </form>
-            
-        </div>
-    </form>
-
-    <br><br>
-
-    <form action="./index.php" method="POST">
-        <div class="topnav3">
-            <form id="questionnaire" action="./index.php" method='GET'>
-                <input type="button" name="action" value="questionnaire" />
-            </form>
-            
-        </div>
-    </form>
-    <br><br>
-
-    <form action="./index.php" method="POST">
-        <div class="topnav3">
-            <form id="consultation" action="./index.php" method='GET'>
-                <input type="button" name="action" value="consultation" />
-            </form>
-            
-        </div>
-    </form>
+<form action="./index.php" method="GET">
+            <button type="submit" value="profil" name="action" class="btn btn-primary">Profil</button>
+    </form><br><br>
+    <form action="./index.php" method="GET">
+            <button type="submit" value="liste_dossier_patient" name="action" class="btn btn-primary">Liste des patients</button>
+    </form><br>
 </body>
 </html>
