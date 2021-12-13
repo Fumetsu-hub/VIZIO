01b2ba64066f64240738ee86836b97aa61802cdb
Ruben <rubenh4502@gmail.com>
2021-12-13 01:28:45 +0100

    ruben

--- view/accueil.php
+++ view/accueil.php
@@ -10,18 +10,25 @@
 include('./utilitaire/bootstrap.php');
 include('./utilitaire/barre_de_navigation.php');
 ?>
-
-<br><br><br><br><br>
-<div class = "container">
-    <form action="./index.php" method="GET">
-            <button type="submit" value="creer_dossier_patient" name="action" class="btn btn-primary">Crée un dossier patient</button>
-    </form><br><br>
-    <form action="./index.php" method="GET">
-            <button type="submit" value="consultation" name="action" class="btn btn-primary">Crée une fiche consultation</button>
-    </form><br><br>
-    <form action="./index.php" method="GET">
-            <button type="submit" value="questionnaire" name="action" class="btn btn-primary">Crée un questionnaire</button>
-    </form><br>
+<br><br>
+<div class="card container shadow" style="border-width: 5px; width: 27rem; background-color:white;"><br>
+  <img class="card-img-top" src="./utilitaire/image.png" alt="Card image cap">
+  <div class="card-body">
+    <p style="text-align:center; font-weight: bold; font-size:25px; color:black;" class="card-text">Actions rapides</p><hr><br>
+        <div class = "container">
+        <form action="./index.php" method="GET">
+                <button type="submit" value="creer_dossier_patient" name="action" class="btn btn-primary">Crée un dossier patient</button>
+        </form><br>
+        <form action="./index.php" method="GET">
+                <button type="submit" value="consultation" name="action" class="btn btn-primary">Crée une fiche consultation</button><a> (à venir)</a>
+        </form><br>
+        <form action="./index.php" method="GET">
+                <button type="submit" value="questionnaire" name="action" class="btn btn-primary">Crée un questionnaire</button><a> (à venir)</a>
+        </form>
+        </div><br>
+        <hr>
+  </div>
 </div>
+<br>
 </body>
 </html>
