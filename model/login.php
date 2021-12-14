0214574c8476ab0f35491b0f24b39015436192e3
Ruben <rubenh4502@gmail.com>
2021-12-14 19:02:27 +0100

    ruben

--- model/login.php
+++ model/login.php
@@ -2,8 +2,7 @@
 
 <?php
 // connexion à la base de données
-$base = mysqli_connect('localhost', 'root', '');
-mysqli_select_db($base, "vizio");
+include('./utilitaire/db_connexion.php');
 ?>
 
  <?php
