cf6f1bef6f88a4104ee441a0b8a7f2a429c2b058
Angel <coelhopereira@et.intechinfo.fr>
2022-01-03 12:30:29 +0100

    angel

--- model/ajouter_docu.php
+++ model/ajouter_docu.php
@@ -4,3 +4,10 @@
 // connexion à la base de données
 include('./utilitaire/db_connexion.php');
 ?>
+
+<?php
+ $id_img = $_SESSION["id_user"];
+ $sql = 'INSERT INTO document (doc_name, doc_bin) VALUES ($_FILES['userfile'],$_FILES['size']) WHERE doc = '.id_img.'';
+ $img_result = mysqli_query($base, $sql_img) or die("Erreur SQL !<br />".$sql.'<br />'.mysqli_error($base));
+
+ ?>
