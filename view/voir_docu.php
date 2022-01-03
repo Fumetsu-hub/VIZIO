f939cdd3485c9720a977ca3daab55a6fd2f7ceca
Angel <coelhopereira@et.intechinfo.fr>
2022-01-03 16:38:58 +0100

    angel

--- view/voir_docu.php
+++ view/voir_docu.php
@@ -13,13 +13,16 @@ include('./utilitaire/barre_de_navigation.php');
 ?>
 <div class="responsive_card card border-0 shadow rounded-3 my-5 container">
   <div class="card-body">
-
+<?php
+ echo  $_SESSION["id_user"];
+  ?>
 
   <br>
     <form action="./index.php" method="GET">
             <button type="submit" value="ajouter" name="action" class="btn btn-dark">Ajouter document</button>
     </form><br>
     <br>
+  
     <form action="./index.php" method="GET">
             <button type="submit" value="afficher_dossier_patient" name="action" class="btn btn-dark">Retour</button>
     </form><br>
