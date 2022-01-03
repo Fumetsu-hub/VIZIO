ba92304c8ece3a0fae6a67baf5c27e58398e9070
Angel <coelhopereira@et.intechinfo.fr>
2022-01-03 14:47:59 +0100

    angel

--- model/supprimer_docu.php
+++ model/supprimer_docu.php
@@ -3,7 +3,7 @@ include('./utilitaire/db_connexion.php');
 
 $id_user = $_SESSION["id_user"]; // récupère l'id de l'utilisateur
 
-$requete = "DELETE FROM `document` WHERE doc = $id_patient";
+$requete = "DELETE FROM `document` WHERE doc = $id_user";
 mysqli_query($base, $requete) or die('Erreur : '.$requete.'<br />'.mysqli_error($base));
 
 include('./controlleur/voir_docu.php'); //renvoi vers la liste document
