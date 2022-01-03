037e489ac16f447fd22389ba0859fe3c4e05040c
Angel <coelhopereira@et.intechinfo.fr>
2022-01-03 12:33:26 +0100

    angel

--- model/ajouter_docu.php
+++ model/ajouter_docu.php
@@ -7,7 +7,7 @@ include('./utilitaire/db_connexion.php');
 
 <?php
  $id_img = $_SESSION["id_user"];
- $sql = 'INSERT INTO document (doc_name, doc_bin) VALUES ($_FILES['userfile'],$_FILES['size']) WHERE doc = '.id_img.'';
+ $sql = 'INSERT INTO document (doc_name, doc_bin) VALUES ($_FILES['userfiles'],$_FILES['size']) WHERE doc = '.id_img.'';
  $img_result = mysqli_query($base, $sql_img) or die("Erreur SQL !<br />".$sql.'<br />'.mysqli_error($base));
 
  ?>
