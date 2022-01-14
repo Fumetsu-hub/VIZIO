1e379629ed511b3fd0f5cc34f4b7ca602f2fbe2a
Angel <coelhopereira@et.intechinfo.fr>
2022-01-14 10:32:09 +0100

    angel

--- model/ajouter_docu.php
+++ model/ajouter_docu.php
@@ -34,35 +34,31 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
                 }
         }
  }     
-
+$req = $base->query('INSERT INTO document (doc_bin) VALUES(site/)');
  
- {
+ 
+ 
+ //if(isset($_POST["submit"])){
      
- $req = $base->query('INSERT INTO document (doc_bin) VALUES(?)');
- 
- }
- 
- if(isset($_POST["submit"])){
-     
-     echo(var_dump($_FILES,$_POST));
-    $b = getimagesize($_FILES["monfichier"]["tmp_name"]);
+   //  echo(var_dump($_FILES,$_POST));
+    //$b = getimagesize($_FILES["monfichier"]["tmp_name"]);
     //Vérifiez si l'utilisateur à sélectionné une image
-    if($b !== 0){
-        echo("tata");
+    //if($b !== 0){
+      //  echo("tata");
         //Récupérer le contenu de l'image
-        $file = $_FILES['monfichier']['tmp_name'];
-        $image = addslashes(file_get_contents($file));
+        //$file = $_FILES['monfichier']['tmp_name'];
+        //$image = addslashes(file_get_contents($file));
 
 
-        $query = $db->query("INSERT into document (doc_bin) VALUES ('$image')");
-        if($query){
-            echo "Fichier uploadé avec succès.";
-        }else{
-            echo "Échec d'upload du fichier.";
-        } 
-    }else{
-        echo "Veuillez sélectionner une image à uploader.";
-    }
-}
+        //$query = $db->query("INSERT into document (doc_bin) VALUES ('$image')");
+        //if($query){
+          //  echo "Fichier uploadé avec succès.";
+        //}else{
+         //   echo "Échec d'upload du fichier.";
+        //} 
+    //}else{
+      //  echo "Veuillez sélectionner une image à uploader.";
+    //}
+//}
 header("Refresh:0; url=../index.php?action=Document");
 ?>
