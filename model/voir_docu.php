60548ac0ef4538aaa56b02c7f9f9d9e7db57d5b3
Angel <coelhopereira@et.intechinfo.fr>
2022-01-14 09:56:18 +0100

    angel

--- model/voir_docu.php
+++ model/voir_docu.php
@@ -1,20 +1,2 @@
 <!DOCTYPE html>
 
-<?php
-// connexion à la base de données
-include('./utilitaire/db_connexion.php');
-?>
-
- <?php
- $id_img = $_SESSION["id_user"];
- $sql_img = "SELECT doc_bin FROM document WHERE id = '$id_img'";
- $img_result =$base->query($sql_img);
- if($img_result->num_rows>0){
-    $toto=$img_result->fetch_assoc();
-    echo '<img src="data:image/jpeg;base64,'.base64_encode( $toto['doc_bin'] ).'"/>';
- }
- else{
-     echo("probleme");
- }
- ?>
- 
--- view/voir_docu.php
+++ view/voir_docu.php
@@ -11,10 +11,26 @@ include('./utilitaire/bootstrap.php');
 //navbar
 include('./utilitaire/barre_de_navigation.php');
 ?>
+
 <div class="responsive_card card border-0 shadow rounded-3 my-5 container">
   <div class="card-body">
 
-
+  <?php
+// connexion à la base de données
+include('./utilitaire/db_connexion.php');
+?>
+<?php
+ $id_img = $_SESSION["id_user"];
+ $sql_img = "SELECT doc_bin FROM document WHERE id = '$id_img'";
+ $img_result =$base->query($sql_img);
+ if($img_result->num_rows>0){
+    $toto=$img_result->fetch_assoc();
+    echo '<img src="data:image/jpeg;base64,'.base64_encode( $toto['doc_bin'] ).'"/>';
+ }
+ else{
+     echo("probleme");
+ }
+ ?>
   <br>
     <form action="./index.php" method="GET">
             <button type="submit" value="ajouter" name="action" class="btn btn-dark">Ajouter document</button>
