0a85b6e944b3537a5fd9271b53facf2553674501
Ruben <rubenh4502@gmail.com>
2022-01-17 12:29:30 +0100

    ruben

---
+++ sql/create_table_document.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `document` ;
+CREATE TABLE `document` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image` longblob NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
