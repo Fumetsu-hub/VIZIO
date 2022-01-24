d5ff3dd75a22e907352589ac02d6f5c652921736
Angel <coelhopereira@et.intechinfo.fr>
2022-01-24 14:25:04 +0100

    angel

--- model/ajouter_docu.php
+++ model/ajouter_docu.php
@@ -20,7 +20,7 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
                 if(in_array($infosfichier, $extensions_autorisees)){
                   $image = $_FILES['monfichier']['tmp_name'];
                   $imgContent = addslashes(file_get_contents($image));
-                  $insert = $base->query("INSERT into document (image, created) VALUES ('$imgContent', NOW())");
+                  $insert = $base->query("INSERT into document (doc_bin, created) VALUES ('$imgContent', NOW())");
                   if($insert){ 
                     $status = 'success'; 
                     $statusMsg = "Document enregistré."; 
@@ -52,9 +52,9 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
                         
                          
  
-                }
+                
         //}
- }     
+     
 //$req = $base->query('INSERT INTO document (doc_bin) VALUES('$doc_bin')');
  
  
