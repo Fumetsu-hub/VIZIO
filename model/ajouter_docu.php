7b7981cf9ef18b6591d5a1d6e39bde998c006698
Angel <coelhopereira@et.intechinfo.fr>
2022-01-25 10:38:37 +0100

    angel

--- model/ajouter_docu.php
+++ model/ajouter_docu.php
@@ -6,9 +6,47 @@
  mysqli_select_db($base, "vizio");
 ?>
 <?php
+$status = $statusMsg = ''; 
+if(isset($_POST["submit"])){ 
+    $status = 'error'; 
+    if(!empty($_FILES["image"]["name"])) { 
+        // Get file info 
+        $fileName = basename($_FILES["image"]["name"]); 
+        $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+         
+        // Allow certain file formats 
+        $allowTypes = array('jpg','png','jpeg','gif'); 
+        if(in_array($fileType, $allowTypes)){ 
+            $image = $_FILES['image']['tmp_name']; 
+            $imgContent = addslashes(file_get_contents($image)); 
+         
+            // Insert image content into database 
+            $insert = $base->query("INSERT into document (image, created) VALUES ('$imgContent', NOW())"); 
+             
+            if($insert){ 
+                $status = 'success'; 
+                $statusMsg = "Document enregistré."; 
+            }else{ 
+                $statusMsg = "Echec de l'enregistrement du document."; 
+            }  
+        }else{ 
+            $statusMsg = 'Seulement les documents JPG, JPEG, PNG, & GIF sont acceptées.'; 
+        } 
+    }else{ 
+        $statusMsg = 'Veuillez selectionner un document à enregistrer.'; 
+    } 
+} 
+ 
+// Display status message 
+echo $statusMsg; 
 
+?>
 
-if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
+<form action="./document_accueil.php" method="GET" class="mt-4">
+    <button type="submit" value="ques_rep" name="action" class="btn btn-primary">Retour</button><br>
+</form>
+<?php
+/*if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
 {
         // Testons si le fichier n'est pas trop gros
         if ($_FILES['monfichier']['size'] <= 10000000)
@@ -82,4 +120,5 @@ if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
     //}
 //}
 header("Refresh:0; url=../index.php?action=Document");
+?>*/
 ?>
